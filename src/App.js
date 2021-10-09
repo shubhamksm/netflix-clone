49f399377ee033b60fa341670153497e0633fc90
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2021-10-09 11:38:30 +0530

    Added base style index.scss, favicon, logo ressources, global variables

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <i class="fas fa-thumbs-up fa-5x"></i>
       </header>
     </div>
   );

e6aadb74242d1e8028f8841cfa07e29566a0c9df
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2022-01-03 23:58:52 +0530

    Refactored styles, added responsiveness

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { get } from 'lodash';
 import { IMAGE_BASE_PATH } from '../../api/constants';
+import './Showcase.scss';
 
 const Showcase = ({ trendingMovies }) => {
 
     const movie = get(trendingMovies, 'results[0]', {});
 
     return <div className="showcase">
+        <div className="showcase__content">
+            <h3>{movie.title}</h3>
+            <p>{movie.overview}</p>
+        </div>
         <img src={`${IMAGE_BASE_PATH}original${movie.backdrop_path}`} />
     </div>
 }

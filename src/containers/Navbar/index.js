476fea11d17c8ee873d3233acbc294b72d488cec
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2022-08-01 13:36:10 +0530

    Added css modifier to change bg color on scroll to navbar

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
 import MainLogo from "../../resources/netflix-main-logo.svg";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [className, setClassName] = useState("");
+
+  const handleScroll = () => {
+    if (document.documentElement.scrollTop > 95) {
+      setClassName("--bgDark");
+    } else {
+      setClassName("");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <div className="navbar">
+    <div className={classNames("navbar", className)}>
       <ul>
         <li className="navbar__logo">
           <img src={MainLogo} alt="Netflix" />
@@ -12,13 +30,13 @@ const Navbar = () => {
       </ul>
       <ul>
         <li>
-          <i class="fas fa-search fa-lg"></i>
+          <i className="fas fa-search fa-lg"></i>
         </li>
         <li>
-          <i class="far fa-bell fa-lg"></i>
+          <i className="far fa-bell fa-lg"></i>
         </li>
         <li>
-          <i class="fas fa-user fa-lg"></i>
+          <i className="fas fa-user fa-lg"></i>
         </li>
       </ul>
     </div>
